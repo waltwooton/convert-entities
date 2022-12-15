bdb607afa048dd5e00bb3b6bbab79b5f2d010ff2
waltwooton <60298282+waltwooton@users.noreply.github.com>
2022-12-15 09:01:35 -0500

    Add files via upload

---
+++ convertEntities.js
@@ -0,0 +1,30 @@
+const convert = (str) => {
+	//adapted from https://stackoverflow.com/questions/18749591/encode-html-entities-in-javascript/39243641#39243641
+	try { 
+		const heList = require('./htmlentities.json');
+		return str.replace(/\&([^;]+);/g, function (entity, entitycode) {
+			var match;
+			if (entitycode in (heList)) {
+				//console.log("if");
+				//console.log (entity + ": " + entitycode);
+				return (heList[entitycode]);
+			} 
+			else if (match = entity.match(/^#x([\da-fA-F]+)$/)) {
+			   return String.fromCharCode(parseInt(match[1], 16));
+			} 
+			else if (match = entity.match(/^#(\d+)$/)) {
+			   return String.fromCharCode(~~match[1]);
+			} 
+			else {
+				//console.log("else");
+				//console.log (entity + ": " + entitycode);
+				return entity;
+			}
+		})
+	}
+	catch (err) {
+		console.log(`Error reading htmlentities file from disk: ${err}`);
+		return err;
+	}
+}
+exports.convert = convert;
